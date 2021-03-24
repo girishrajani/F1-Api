cbc8621c939c363ed05256a9134bd0277b449068
Girish Rajani <girish.r.rajani@gmail.com>
2021-03-24 16:22:47 +0530

    POST Routes Updated

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+const jsonParser = require('body-parser');
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
+const db = require('./config/db');
 
 const app = express();
-
 const port = 8080;
-require('./app/routes')(app,{});
 
+app.use(jsonParser.urlencoded({extended: true}));
 
 app.set('view engine','ejs')
 
@@ -14,8 +14,10 @@ app.get('/', function(req, res) {
     res.render("index");
 });
 
-
-
-app.listen(port,() => {
-    console.log('Server Listening to: Port'+port)
+MongoClient.connect(db.url, (err,database) => {
+    if (err) return console.log(err);
+    require('./app/routes')(app,{});
+    app.listen(port,() => {
+        console.log('Server Listening to: Port'+port)
+    })
 })
