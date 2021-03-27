aca0b95b5c93433c3bcbd24bc299ea256cd43e7b
Girish Rajani <girish.r.rajani@gmail.com>
2021-03-27 18:47:50 +0530

    Body-Parser Added

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv/config');
 const app = express();
 
 const port = 3000;
+
+app.use(bodyParser.json());
+
 
 mangoose.connect(
     process.env.DB_CONNECTION,
