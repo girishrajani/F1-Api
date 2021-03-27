6190447460a9d33ab307a8228f0b7d7706f7b418
Girish Rajani <girish.r.rajani@gmail.com>
2021-03-27 19:22:13 +0530

    Routes Designed

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 require('dotenv/config');
-const app = express();
 
+const app = express();
 const port = 3000;
+
 
 app.use(bodyParser.json());
 app.set('view engine','ejs')
+
+// Importing Routes
+const driversRoute = require('./routes/driver-championship')
+app.use('/drivers-championships',driversRoute);
+
+const teamsRoute = require('./routes/team-championship')
+app.use('/team-championships',teamsRoute);
 
 app.get('/', function(req, res) {
     res.render("index");
